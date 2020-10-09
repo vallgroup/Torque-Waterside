ac2bc644c65ad0c75cfb63257be5caa5b32cfa5f
Al Notara <al@vallgroup.com>
2020-10-09 12:10:27 +1100

    added Facebook tracking Pixel to website

--- src/functions.php
+++ src/functions.php
@@ -151,6 +151,27 @@ function torque_enqueue_child_scripts() {
 }
 
 
+add_action( 'wp_head', 'waterside_head_output' );
+function waterside_head_output() { ?>
+	<!-- Facebook Pixel Code -->
+	<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+			n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		 n.queue=[];t=b.createElement(e);t.async=!0;
+		 t.src=v;s=b.getElementsByTagName(e)[0];
+		 s.parentNode.insertBefore(t,s)}(window, document,'script',
+										 'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '332649404637194');
+		fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" style="display:none"
+				   src="https://www.facebook.com/tr?id=332649404637194&ev=PageView&noscript=1"
+				   /></noscript>
+	<!-- End Facebook Pixel Code -->
+<?php }
+
 
 function torque_customize_register( $wp_customize ) {
 
