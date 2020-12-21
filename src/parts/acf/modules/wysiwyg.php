4be3fd56fbaad10da43c4877a5d60081099c559b
Mario Vallejo <mario@vallgroup.com>
2020-12-21 10:16:52 -0500

    adds wysiwyg to modules

---
+++ src/parts/acf/modules/wysiwyg.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Content Module Template: Map
+ */
+?>
+
+<section class="wysiwyg-content">
+
+  <div class="row-container">
+
+    <div class="content-container">
+
+      <?php if ( $content ) echo $content; ?>
+
+    </div>
+
+  </div>
+
+</section>
